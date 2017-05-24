4a5be8497fe657d2f69cd645ab97e833ba838002
Swaraj Dhumne <swaraj.dhumne@gmail.com>
2017-05-24 15:07:53 -0400

    finished cookie challenge

---
+++ MouseAndWire/web/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>MouseAndWire</title>
+    </head>
+
+    <style>
+    p {
+        width: 400px;
+    }
+    </style>
+
+    <body>
+
+    <p>Scientists are running an experiment. In a room, there is a mouse and a screen with a wire coming into it. If the screen displays an item which the mouse does not want, then the mouse will chew through the wire! The problem is, the technician only comes once every 60 seconds, and we know he clocked in and fixed the wire at 1pm sharp! Help the scientists figure out what the mouse wants.</p>
+
+    <?php 
+    
+    if(array_key_exists("mouse_wants", $_COOKIE) && preg_match('/([Cc])(ookie)+/', $_COOKIE["mouse_wants"])){
+        echo('<p>You got it! Flag: glass_of_milk</p>');
+    } else{
+        echo("<p>What does the mouse want?</p>");
+        $t = date("h:i:sa", time());
+        echo("<div> it is now: $t </div>");
+        if (time() % 60 == 0) {
+            setcookie("mouse_wants", 'something', time()+1);
+        }
+    }
+    
+    ?>
+    <img src="mouse.jpg" alt="">
+    </body>
+
+</html>
