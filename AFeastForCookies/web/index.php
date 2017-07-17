cfc8901e4822bfb20ba70bd310164ec79b2f3cf7
Matthew Blumen <matthb7@vt.edu>
2017-07-17 00:58:00 -0400

    Update index.php
    Fix issues

--- AFeastForCookies/web/index.php
+++ AFeastForCookies/web/index.php
@@ -15,17 +15,16 @@
         if (isset($_POST['cookies'])) {
             $val = $_POST['cookies'];
         }
-		$message = 'WW91IGdvdCBpdCEgRmxhZzogY29va2llX291c3NpZGVfaG93X2JvdXRfZGF0==';
-		setcookie("url_decode_this", $message, time() + 3600);
+		$message = 'WW91IGdvdCBpdCEgRmxhZzogc2Vzc2lvbl9jb29raWVzX3dpdGhfcmFpc2lucw==';
+		setcookie("ruh_roh_session", $message, time() + 7200);
 	?>
 
     <body>
 
     	<p>
-    		Shaggy and Scooby lost all of their Scooby Snacks. They might look into trying cookies instead. What kind of cookies should they get?
-    	</p>
-	    
-		<h3>What kind of cookies should they get?</h3>
+    		Shaggy and Scooby ate all of the Scooby Snacks and are still hungry. Let's see what kind of other snacks they can find!
+	    </p>
+		<h3>What kind of snacks did they find?</h3>
 	    <form action="" method="post">
 			<input type="text" name="cookies" value="<?php echo $val; ?>">
 			<input type="submit" value="Submit">
