3ef4ed0fa95e5b7533f17201028dfb8071c2da88
Swaraj Dhumne <swaraj.dhumne@gmail.com>
2017-07-20 22:08:23 -0400

    reworked crosscook a bit

--- CrossCook/web/index.php
+++ CrossCook/web/index.php
@@ -1,47 +1,6 @@
 <?php
-function encrypt($str) {
 
-    $str = strtolower($str);
-
-    $sessid = "c353936a6883a66379f45404fb048c92";
-    $strln = strlen($str);
-
-    //I can do this because the str is always the sessid lol
-    for ($i = 0; $i < $strln; $i++) {
-        $encordval = 0;
-        $strordval = 0;
-        $str_is_num = false;
-        $newchar = "";
-
-        $cur_enc_char = substr($sessid, $i, 1);
-        if (is_numeric($cur_enc_char)) {
-            $encordval = (((int)$cur_enc_char + 26) % 26) + 97;
-        } else {
-            $encordval = ord($cur_enc_char);
-        }
-
-
-        $cur_str_char = substr($str, $i, 1);
-        if (is_numeric($cur_str_char)) {
-            $strordval = (int)$cur_str_char;
-            $str_is_num = true;
-        } else {
-            $strordval = ord($cur_str_char);
-        }
-
-        if ($str_is_num) {
-            $newchar = (string)(($strordval + $encordval) % 9); 
-        } else {
-            $newchar = chr(((($encordval - 97) + ($strordval - 97)) % 26) + 97);
-        }
-
-        // echo(($encordval - 97) . " . " . ($strordval - 97) . " . " . chr($newchar) . "<br/>");
-        $str[$i] = $newchar;
-    }
-    return $str;
-}
-
-setcookie("helping_hand", "c353936a6883a66379f45404fb048c92");
-
-echo("flag is: " . encrypt("61f6933f179df0c61cba100995e7ebea"));
+setcookie("subtract_this", "c353936a6883a66379f45404fb048c92");
+echo("flag is: " . "61m6933r179pr0j61onm100995l7qnqh");
+echo("hashes have both letters and numbers, but most ciphers deal with only letters..");
 ?>
