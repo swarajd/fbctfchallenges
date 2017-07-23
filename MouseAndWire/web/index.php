8a2da89e4dfda28233c12d46b9d937592ffba5e7
Swaraj Dhumne <swaraj.dhumne@gmail.com>
2017-07-22 20:52:50 -0400

    fixed cookie issue, fixed sql issue

--- MouseAndWire/web/index.php
+++ MouseAndWire/web/index.php
@@ -1,3 +1,15 @@
+<?php 
+    $got_it = false;
+    if(array_key_exists("mouse_wants", $_COOKIE) && preg_match('/([Cc])(ookie)+/', $_COOKIE["mouse_wants"])){
+        $got_it = true;
+    } else{
+        $got_it = false;
+        if (time() % 60 == 0) {
+            setcookie("mouse_wants", 'something', time()+1);
+        }
+    }
+?>
+
 <html lang="en">
     <head>
         <title>MouseAndWire</title>
@@ -8,19 +20,16 @@
     <body>
         <div class="page">
             <p>Scientists are running an experiment. In a room, there is a mouse and a screen with a wire coming into it. If the screen displays an item which the mouse does not want, then the mouse will chew through the wire! The problem is, the technician only comes once every 60 seconds, and we know he clocked in and fixed the wire at 1pm sharp! Help the scientists figure out what the mouse wants.</p>
-
-            <?php 
-                if(array_key_exists("mouse_wants", $_COOKIE) && preg_match('/([Cc])(ookie)+/', $_COOKIE["mouse_wants"])){
+            <?php
+                if ($got_it) {
                     echo('<p>You got it! Flag: glass_of_milk</p>');
-                } else{
+                } else {
                     echo("<p>What does the mouse want?</p>");
                     $t = date("h:i:sa", time());
                     echo("<div> it is now: $t </div>");
-                    if (time() % 60 == 0) {
-                        setcookie("mouse_wants", 'something', time()+1);
-                    }
                 }
             ?>
+            
             <img src="./css/picture.jpg" alt="picture">
         </div>
     </body>
